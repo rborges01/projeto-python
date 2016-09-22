4505db60a4c86cccd7ac0e407a54965de49bf421
Renato Borges <renato.nascimento@corp.globo.com>
2016-09-22 15:26:22 -0300

    Arquivo do servidor python

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+# config valid only for current version of Capistrano
+require 'capistrano/ext/multistage' 
+
+set :stages, %w(production)
+set :default_stage, "production" 
+
+set :application, 'myproject.py'
+set :user, 'renato'
+set :use_sudo, false
+
+# setup repo details
+set :scm, :git
+set :repository, 'git@github.com:rborges01/projeto-python.git'
+set :deploy_to, "/root/myproject/" 
+
+# how many old releases do we want to keep
+set :keep_releases, 2
+
+namespace :deploy do
+  after :finishing, 'deploy:cleanup'
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '10.2.45.83',  :app, primary: true

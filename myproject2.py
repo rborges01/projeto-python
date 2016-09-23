e74cee221d42a1feadfdfa246e42be833c10951c
Renato Borges <renato.nascimento@corp.globo.com>
2016-09-23 11:54:40 -0300

    Teste com varias palavras

--- myproject2.py
+++ myproject2.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
    
- return "<h1 style='color:blue'>Ola mundo!</h1>"
+ return "<h1 style='color:blue'>Ola mundo! Esse foi meu deploy para o servidor python</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=5002)
